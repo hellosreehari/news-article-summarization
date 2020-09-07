14f0c754ddb4556463492776a5ea1f7d7e1400b7
Sreehari A <sreeharianilkumar1@gmail.com>
2020-09-07 12:16:30 +0530

    Update settings.py

--- ShortNews/settings.py
+++ ShortNews/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-hgd2=##h^s%itzg#w%nei)&j6@e$fu#l&7ns9syxz$5nub3)a'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
